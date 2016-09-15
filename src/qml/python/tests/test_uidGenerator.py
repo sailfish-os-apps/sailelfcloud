fdf99838c26dec807d61b6d87e03361b2748ea44
Teemu Ahola <teemuahola7@gmail.com>
2016-09-15 22:00:19 +0300

    Added UID generator and unit tests for it.

---
+++ src/qml/python/tests/test_uidGenerator.py
@@ -0,0 +1,56 @@
+'''
+Created on Sep 15, 2016
+
+@author: teemu
+'''
+import unittest
+import threading
+import uidGenerator
+
+
+class TestUidGenerator(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_getUid_ShouldReturnUniqueId(self):
+        self.assertEqual(1, uidGenerator.getUid())
+        self.assertEqual(2, uidGenerator.getUid())
+        self.assertEqual(3, uidGenerator.getUid())
+
+    parallelTasks = []
+    parallelResults = []
+
+    def async_getUid(self):
+        t = threading.Thread(target = lambda : self.parallelResults.append(uidGenerator.getUid()))
+        t.start()
+        self.parallelTasks.append(t)
+
+    @staticmethod
+    def unique_values(g):
+        s = set()
+        for x in g:
+            if x in s: return False
+            s.add(x)
+        return True
+
+    def test_getUid_Threaded_ShouldReturnUniqueId(self):
+
+        for _ in range(1000):
+            self.async_getUid()
+
+        for t in self.parallelTasks:
+            t.join()
+
+        self.assertTrue(TestUidGenerator.unique_values(self.parallelResults), "failed to generate unique IDs in parallel")
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ src/qml/python/uidGenerator.py
@@ -0,0 +1,31 @@
+'''
+Created on Sep 15, 2016
+
+@author: teemu
+'''
+
+import threading
+
+def atomic(func):
+    from functools import wraps
+    @wraps(func)
+    def _atomically(self, *args, **kwargs):
+        with self.lock:
+            return func(self, *args, **kwargs)
+    return _atomically
+
+class UidGenerator(object):
+
+    def __init__(self, value=0):
+        self.lock = threading.Lock()
+        self.value = value
+
+    @atomic
+    def get(self):
+        self.value += 1
+        return self.value
+
+UID_GENERATOR = UidGenerator()
+
+def getUid():
+    return UID_GENERATOR.get()
