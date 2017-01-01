1994bb7886d283032205862b47c0227f2d0fecea
Teemu Ahola <teemuahola7@gmail.com>
2017-01-01 11:57:59 +0200

    Implementing file based logging.

--- src/SailElfCloud.cpp
+++ src/SailElfCloud.cpp
@@ -1,12 +1,57 @@
+#include <QtGlobal>
 #include <QtQuick>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QStandardPaths>
 #include <sailfishapp.h>
 #include "Helpers.h"
 
+static void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QDateTime local(QDateTime::currentDateTime());
+    QDateTime UTC(local.toTimeSpec(Qt::UTC));
+    QString output;
+
+    switch (type) {
+    case QtDebugMsg:
+        output =+ "[D]";
+        break;
+    case QtWarningMsg:
+        output += "[W]";
+        break;
+    case QtCriticalMsg:
+        output += "[C]";
+        break;
+    case QtFatalMsg:
+        output += "[F]";
+        break;
+    default:
+        output += "[?]";
+    }
+
+    output += " " + msg + " (" + context.file + ":" + QString::number(context.line) + ", " + context.function + ") [" + UTC.toString("yyyy.MM.dd|hh:mm:ss") + "]";
+
+#ifdef QT_DEBUG
+    fprintf(stderr, "%s\n", output.toLocal8Bit().data());
+#endif
+
+    const QString logPath = QStandardPaths::standardLocations(QStandardPaths::CacheLocation)[0] + "/logs";
+    QDir dir;
+    dir.mkpath(logPath);
+    QFile::setPermissions(logPath, QFileDevice::ReadOwner | QFileDevice::WriteOwner | QFileDevice::ExeOwner);
+    QFile logFile(logPath + "/log.txt");
+    logFile.open(QIODevice::ReadWrite | QIODevice::Append | QIODevice::Text);
+    QTextStream out(&logFile);
+    out << output << "\n";
+
+    if (type == QtFatalMsg)
+        abort();
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput);
+
     // Set up qml engine.
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QScopedPointer<QQuickView> v(SailfishApp::createView());
