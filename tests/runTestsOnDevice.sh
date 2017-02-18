6e0f6a9fa25892fda281079b8f3c0eabcc26fc76
Teemu Ahola <teemuahola7@gmail.com>
2017-02-18 13:28:28 +0200

    Changed output format to plain xml.

--- tests/runTestsOnDevice.sh
+++ tests/runTestsOnDevice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script for running tests. That's for specifying just one argument in QtCreator's configuration
-/usr/bin/tst-harbour-sailelfcloud -o result.xml,xunitxml -input /usr/share/tst-harbour-sailelfcloud
+/usr/bin/tst-harbour-sailelfcloud -o result.xml,xml -input /usr/share/tst-harbour-sailelfcloud
 
 # When you'll get some QML components in the main app, you'll need to import them to the test run
 # /usr/bin/tst-harbour-helloworld-pro-sailfish -o result.xml,xunitxml -input /usr/share/tst-harbour-helloworld-pro-sailfish -import /usr/share/harbour-helloworld-pro-sailfish/qml/components
